fb05dec54e6b34f2e5a8f7e1cd223b5d5bb14428
smolk19841984-design <smolk19841984@gmail.com>
2026-04-05 16:31:28 +0500

    Script to delete specified files and directories

---
+++ delete_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Delete specified files and directories
+rm -rf Config/
+rm -rf app/
+rm -rf build/
+rm -rf build_app/
+rm -rf 'build_app — копия/'
+rm -rf darksword_ppl/
+rm -rf *.py
+rm -rf *.sh
+rm -rf *.bat
+rm -rf *.pem
+rm -rf *.plist
+
+# Confirmation of deletion
+echo "Specified files and directories have been deleted."
